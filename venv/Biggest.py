28e80b61ca0d5ced8e224c7850e3bc2da3f16ae3
Sania2001 <saniagshinde@gmail.com>
2022-03-24 17:16:50 +0530

    .

---
+++ venv/Biggest.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+def check(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+
+class biggest(unittest.TestCase):
+
+    def test_biggest(self):
+        a=10
+        b=5
+        z= check(a,b)
+        p= max(a,b)
+        self.assertEqual(p,z)
+
+if __name__ == "__main__":
+    unittest.main()
+
