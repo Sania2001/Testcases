9f1dba967af71597572032b50ca8bcdecaf58a90
Sania2001 <saniagshinde@gmail.com>
2022-03-25 14:43:54 +0530

    .

--- venv/UnitestingExamples.py
+++ venv/UnitestingExamples.py
@@ -4,7 +4,7 @@
 #
 # result = add(2,3)
 # print (result)
-
+import sys
 import unittest
 
 def add(x, y):
@@ -14,12 +14,14 @@ def add(x, y):
 
 class MyApp1(unittest.TestCase):
 
+    @unittest.skipUnless(sys.platform.startswith("darwin"),"require windows os")
     def test_case1(self):
         a = 10
         b = 22
         c = add(a, b)
         self.assertEqual(32,c)
 
+    @unittest.skip("skipped")
     def test_case2(self):
         a = 5.2
         b = 3.1
